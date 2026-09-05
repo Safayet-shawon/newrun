6fd16cc47cfd15b9a65a1585a8bab4eb4ae3299d
Md.Safayet Hossain Shawon <safayet.shawon16@gmail.com>
2026-09-05 16:47:20 +0600

    Add desktop sidebar to marketplace layout

--- frontend/src/components/marketplace/MarketplaceLayout.jsx
+++ frontend/src/components/marketplace/MarketplaceLayout.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Footer from "@/components/marketplace/Footer";
 import MobileBottomNav from "@/components/marketplace/MobileBottomNav";
 import Header from "@/components/marketplace/Header";
+import DesktopSidebar from "@/components/marketplace/DesktopSidebar";
 
 export default function MarketplaceLayout({ children }) {
   return (
-    <div className="min-h-screen bg-nexora-warm">
-      <Header />
-      <main className="pb-20 md:pb-0">{children}</main>
-      <Footer />
-      <MobileBottomNav />
+    <div className="min-h-screen bg-[#F7FAF8] xl:flex">
+      <DesktopSidebar />
+      <div className="min-w-0 flex-1">
+        <Header />
+        <main className="pb-20 md:pb-0">{children}</main>
+        <Footer />
+        <MobileBottomNav />
+      </div>
     </div>
   );
 }

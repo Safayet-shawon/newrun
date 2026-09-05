1042bc784e8e65d93ecd7b865a0d4154618c3256
Md.Safayet Hossain Shawon <safayet.shawon16@gmail.com>
2026-09-05 16:52:08 +0600

    Add collapsible marketplace sidebar

--- frontend/src/components/marketplace/MarketplaceLayout.jsx
+++ frontend/src/components/marketplace/MarketplaceLayout.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "@/components/marketplace/Footer";
 import MobileBottomNav from "@/components/marketplace/MobileBottomNav";
 import Header from "@/components/marketplace/Header";
 import DesktopSidebar from "@/components/marketplace/DesktopSidebar";
 
 export default function MarketplaceLayout({ children }) {
+  const [desktopSidebarOpen, setDesktopSidebarOpen] = useState(true);
+
   return (
     <div className="min-h-screen bg-[#F7FAF8] xl:flex">
-      <DesktopSidebar />
+      {desktopSidebarOpen && <DesktopSidebar onClose={() => setDesktopSidebarOpen(false)} />}
       <div className="min-w-0 flex-1">
-        <Header />
+        <Header
+          desktopSidebarOpen={desktopSidebarOpen}
+          onToggleDesktopSidebar={() => setDesktopSidebarOpen((v) => !v)}
+        />
         <main className="pb-20 md:pb-0">{children}</main>
         <Footer />
         <MobileBottomNav />
